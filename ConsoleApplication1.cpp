926954114a81c1916f45211e941fafdbd531b31d
Oksana Marina <olmarina@edu.hse.ru>
2016-04-08 17:36:49 +0300

    01

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "Hello, World!"; 
+	return 0;
+}
+
